bc6c402889b9e1a3fea7599867b62f353fbf2c09
+ <+>
2023-09-30 18:03:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const submitData = function(name,email) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name:name,
+            email:email,
+        }),
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data); // Log the response data to understand its structure
+        const bodyElement = document.querySelector("body");
+        // Assuming data.id is a string or number, set it as innerHTML
+        bodyElement.innerHTML = data.id;
+    })
+    .catch(error => {
+        alert("'Unauthorized Access");
+        console.log(error.message);
+    });
+    return fetch;
+};
+submitData("natalie","natalie@gmail.com.com");
